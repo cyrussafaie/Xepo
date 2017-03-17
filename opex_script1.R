3c2be5abbf9919603ba3ad5847a7aea1805b9eb5
Cyrus Safaie <safaie_h@yahoo.com>
2017-03-17 00:36:22 -0500

    date structure fixed

--- opex_script1.R
+++ opex_script1.R
@@ -1,6 +1,29 @@
-eod_inventory=read.csv("eod_inventory.csv")
+library(read)
+?scan
+
+# data stored 
 product_details=read.csv("product_details.csv")
+eod_inventory=read.csv("eod_inventory.csv")
 product_movement=read.csv("product_movement.csv")
+
+#date transfer
+eod_inventory$date<- strptime(eod_inventory$date, "%m/%d/%Y")
+product_movement$date<- strptime(product_movement$date, "%m/%d/%Y")
+
+# data structure 
 str(product_details)
 str(eod_inventory)
 str(product_movement)
+
+# names in each data set
+head(product_details)
+head(eod_inventory)
+head(product_movement)
+
+# summary
+summary(product_details,maxsum=100)
+summary(eod_inventory)
+summary(product_movement)
+
+
+
