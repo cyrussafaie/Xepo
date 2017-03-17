214e0410b852e6170e21f1f7e682518b9362b0bd
Cyrus Safaie <safaie_h@yahoo.com>
2017-03-17 18:14:02 -0500

    format fixed

--- opex_script1.R
+++ opex_script1.R
@@ -1,5 +1,4 @@
-library(read)
-?scan
+
 
 # data stored 
 product_details=read.csv("product_details.csv")
@@ -7,8 +6,14 @@ eod_inventory=read.csv("eod_inventory.csv")
 product_movement=read.csv("product_movement.csv")
 
 #date transfer
+#dat fix
 eod_inventory$date<- strptime(eod_inventory$date, "%m/%d/%Y")
 product_movement$date<- strptime(product_movement$date, "%m/%d/%Y")
+
+#material fix
+product_details$material=as.factor(product_details$material)
+eod_inventory$material=as.factor(eod_inventory$material)
+product_movement$material=as.factor(product_movement$material)
 
 # data structure 
 str(product_details)
@@ -24,6 +29,4 @@ head(product_movement)
 summary(product_details,maxsum=100)
 summary(eod_inventory)
 summary(product_movement)
-
-
 
