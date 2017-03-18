1d80d19c0de9dce507079bc9ad1ebf55f0413069
Cyrus Safaie <safaie_h@yahoo.com>
2017-03-17 23:37:46 -0500

    date frame built

--- opex_script1.R
+++ opex_script1.R
@@ -11,9 +11,9 @@ eod_inventory$date<- strptime(eod_inventory$date, "%m/%d/%Y")
 product_movement$date<- strptime(product_movement$date, "%m/%d/%Y")
 
 #material fix
-product_details$material=as.factor(product_details$material)
-eod_inventory$material=as.factor(eod_inventory$material)
-product_movement$material=as.factor(product_movement$material)
+product_details$material=factor(product_details$material)
+eod_inventory$material=factor(eod_inventory$material)
+product_movement$material=factor(product_movement$material)
 
 # data structure 
 str(product_details)
@@ -26,7 +26,22 @@ head(eod_inventory)
 head(product_movement)
 
 # summary
-summary(product_details,maxsum=100)
+summary(product_details)
 summary(eod_inventory)
 summary(product_movement)
+
+#min and max date in the dataset
+paste("first date in the dataset is",min(min(product_movement$date),min(eod_inventory$date)))
+paste("last date in the dataset is",max(max(product_movement$date),max(eod_inventory$date)))
+max(max(product_movement$date),max(eod_inventory$date))-min(min(product_movement$date),min(eod_inventory$date))
+
+#every date in the data
+date.range.array=seq(as.Date("2011-11-30"), as.Date("2014-12-09"), by="days")
+length(date.range.array)
+
+#creating the data frame for the repeat
+date.array.full=rep(date.range.array,length(unique(product_details$material)))
+
+paste("the dataframe has",length(date.array.full),"rows")
+
 
